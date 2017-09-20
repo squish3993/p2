9b68f8456985c0385ef85cb2e1f81802ab54cb7f
squish3993 <jdlewis@wesleyan.edu>
2017-09-20 10:39:27 -0400

    Finsihed HTML and added CSS

--- index.php
+++ index.php
@@ -13,41 +13,51 @@
 	</head>
 
 	<body>
+	<div class="jumbotron container">
+			<h1 class="display-3 text-center">Intervals</h1>
 
-			<h1>Intervals</h1>
+			<form method='Get' class='form-inline text-center'>
+				<div class="form-group">
+					<div class="lineOne">
+					<label for="note">Enter your first note:</label>
+					<input type="text" class="form-control" name='note' id='note' placeholder="C, F, G, Etc">
 
-			<form method='Get'>
+					<select name='accidental' id='accidental' class="form-control">
+						<option value='natural' >Natural</option>
+						<option value='sharp' ># (Sharp)</option>
+						<option value='flat' >b (Flat)</option>
+					</select>
+					<br>
 
-				<label for='note'>Enter your first note</label>
-				<input type='text' name='note' id='note'>
+					</div>
+					<div class="lineTwo">
+					<label for='note'>Enter your second note:</label>
+					<input type='text' class="form-control" name='noteTwo' id='noteTwo' placeholder="C, F, G, Etc">
 
-				<select name='accidental' id='accidental'>
-					<option value='natural' >Natural</option>
-					<option value='sharp' ># (Sharp)</option>
-					<option value='flat' >b (Flat)</option>
-				</select>
-				<br>
+					<select name='accidentalTwo' id='accidentalTwo' class="form-control">
+						<option value='natural' >Natural</option>
+						<option value='sharp' ># (Sharp)</option>
+						<option value='flat' >b (Flat)</option>
+					</select>
+					<br>
 
-				<label for='note'>Enter your second note</label>
-				<input type='text' name='noteTwo' id='noteTwo'>
+					</div>
 
-				<select name='accidentalTwo' id='accidentalTwo'>
-					<option value='natural' >Natural</option>
-					<option value='sharp' ># (Sharp)</option>
-					<option value='flat' >b (Flat)</option>
-				</select>
-				<br>
+					<div class="lineThree">
+					<input type='checkbox' name='octave' id='octave'>
+					<label for='octave'>Add an Octave</label>
+					<br>
 
-				<input type='checkbox' name='octave' id='octave'>
-				<label for='octave'>Add an Octave</label>
-				<br>
+					</div>
 
 
 
-				<input type='submit' class='btn btn-primary btnsmall' value="Find Interval">
-
+					<input type='submit' class='btn btn-primary' value="Find Interval">
+				</div>
 			</form>
 
-			
+		</div>
+
+		
 	</body>
 </html>
