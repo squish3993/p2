8ee357f4a5bcc82f727225c8128f5ae20acfebc9
squish3993 <jdlewis@wesleyan.edu>
2017-09-20 15:12:13 -0400

    Fixed error messages for error cases

--- intervalsLogic.php
+++ intervalsLogic.php
@@ -5,6 +5,8 @@ require('helpers.php');
 
 $noteValues = ['c' => 0, 'd' => 2, 'e' => 4, 'f' => 5, 'g' => 7, 'a' => 9, 'b'=>11];
 
+$intervalNames = ['unison', 'Minor 2nd', 'Major 2nd', 'Minor 3rd', 'Major 3rd', 'Perfect 4th', 'Tri-Tone (Also called Augmented 4th or Diminished 5th)', 'Perfect 5th', 'Minor 6th', 'Major 6th', 'Minor 7th', 'Major 7th', 'Octave', 'Minor 9th', 'Major 9th', 'Minor 10th', 'Major 10th', 'Perfect 11th', 'Augmented 11th or Dimished 12th', 'Perfect 12th', 'Minor 13th', 'Major 13th', 'Minor 14th', 'Major 14th'];
+
 if (isset($_GET['note'])) {
     $note = $_GET['note'];
 } else {
@@ -36,43 +38,52 @@ if (isset($_GET['octave'])) {
 }
 
 
-$noteNum = $noteValues[strtolower($note)];
-$noteNumTwo = $noteValues[strtolower($noteTwo)];
-
-if ($accidental=='sharp') {
-	$noteNum+=1;
-}	elseif ($accidental == 'flat') {
-	$noteNum-=1;
-} 	else {
-	$noteNum=$noteNum;
-}
-
-if ($accidentalTwo=='sharp') {
-	$noteNumTwo+=1;
-}	elseif ($accidentalTwo == 'flat') {
-	$noteNumTwo-=1;
-} 	else {
-	$noteNumTwo=$noteNumTwo;
-}
 
 
-if (!$octave) {
-	$interval = $noteNumTwo - $noteNum;
-} else {
-	$interval = ($noteNumTwo+12) - $noteNum;
-}
 
-$intervalNames = ['unison', 'Minor 2nd', 'Major 2nd', 'Minor 3rd', 'Major 3rd', 'Perfect 4th', 'Tri-Tone (Also called Augmented 4th or Diminished 5th)', 'Perfect 5th', 'Minor 6th', 'Major 6th', 'Minor 7th', 'Major 7th', 'Octave', 'Minor 9th', 'Major 9th', 'Minor 10th', 'Major 10th', 'Perfect 11th', 'Augmented 11th or Dimished 12th', 'Perfect 12th', 'Minor 13th', 'Major 13th', 'Minor 14th', 'Major 14th'];
 
-$intervalName = $intervalNames[$interval];
+
 if (($note == '') || ($noteTwo == '')) {
 	$alertType ='alert-danger';
 	$results ='Please enter two notes';
 } elseif ((!array_key_exists(strtolower($note), $noteValues)) || (!array_key_exists(strtolower($noteTwo), $noteValues ))) {
 	$alertType='alert-danger';
-	$results='Please enter a valid musical note';
+	$results='Please enter valid musical notes';
 } elseif ((array_key_exists(strtolower($note), $noteValues)) && (array_key_exists(strtolower($noteTwo), $noteValues ))) {
+
+		$noteNum = $noteValues[strtolower($note)];
+		$noteNumTwo = $noteValues[strtolower($noteTwo)];
+
+		if ($accidental=='sharp') {
+			$noteNum+=1;
+			$accidentalChar = '#';
+		}	elseif ($accidental == 'flat') {
+			$noteNum-=1;
+			$accidentalChar= 'b';
+		} 	else {
+			$noteNum=$noteNum;
+			$accidentalChar = '';
+		}
+
+		if ($accidentalTwo=='sharp') {
+			$noteNumTwo+=1;
+			$accidentalCharTwo = '#';
+		}	elseif ($accidentalTwo == 'flat') {
+			$noteNumTwo-=1;
+			$accidentalCharTwo = 'b';
+		} 	else {
+			$noteNumTwo=$noteNumTwo;
+			$accidentalCharTwo = '';
+		}
+
+
+		if (!$octave) {
+			$interval = $noteNumTwo - $noteNum;
+		} else {
+			$interval = ($noteNumTwo+12) - $noteNum;
+		}
+	$intervalName = $intervalNames[$interval];
 	$alertType='alert-info';
-	$results='Your interval is a ' .$intervalName;
+	$results='The interval between ' .strtoupper($note) .$accidentalChar  .' and ' .strtoupper($noteTwo) .$accidentalCharTwo  .' is a ' .$intervalName;
 }
 
